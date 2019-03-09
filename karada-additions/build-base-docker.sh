992d85b178cfbdbfe4ec97c687b0126955e4656c
Max Cantor (home linux) <i@cantor.mx>
2019-03-08 19:43:31 -0800

    breaking docker file into core and additions;

---
+++ karada-additions/build-base-docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+$(aws ecr get-login --no-include-email --region us-west-2 --profile karada-api) 
+if [ -z ${BASE_TAG+x} ]; then BASE_TAG=latest; fi
+
+echo $BASE_TAG
+cd ..
+docker build -t 840787491930.dkr.ecr.us-west-2.amazonaws.com/ap_karada_root:${BASE_TAG} . &&  docker push 840787491930.dkr.ecr.us-west-2.amazonaws.com/ap_karada_root:${BASE_TAG}
+cd -
